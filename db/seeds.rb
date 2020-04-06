ee9d03de3c92495eb89885a3ed8cb80c281527c8
Juan Snyman <git@jsnyman.com>
2020-04-06 22:22:09 +0100

    Change user creation code in seeds.rb to use new user model
    Remove playlist seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,21 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Role.create(name: :admin)
-Role.create(name: :client)
-user1 = User.create(username: 'Nicole',
-								    email: 'admin@gmail.com')
+
+user1 = User.create(provider: 'spotify',
+								    uid: 			'7x4w36usx0owhmmlpbx7khq0f')
 user1.add_role(:admin)
-user2 = User.create(username: 'Bruce',
-								    email: 'client@gmail.com')
-user2.add_role(:client)
-1.upto(5) do |i|
-	Playlist.create(name: "Happy #{i}",
-								 rating: 1 * i,
-								 user: user1)
-end
-1.upto(5) do |i|
-	Playlist.create(name: "Sad #{i}",
-								 rating: 0.5 * i,
-								 user: user2)
-end
+user1.add_role(:standard)
