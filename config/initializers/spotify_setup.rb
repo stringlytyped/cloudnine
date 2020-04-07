0837f6a4bcb4677cc7e3da12c4b75e2c253f3df0
Juan Snyman <git@jsnyman.com>
2020-04-07 19:53:08 +0100

    Remove hard-coded Spotify credentials

--- config/initializers/spotify_setup.rb
+++ config/initializers/spotify_setup.rb
@@ -1,1 +1,2 @@
-RSpotify::authenticate('e324ba6951214b01b54d6100e5049312', 'fba6c1a1f8204bfb974adb028052cd4f')
+RSpotify::authenticate(Rails.application.credentials.spotify[:client_id],
+                       Rails.application.credentials.spotify[:client_secret])
